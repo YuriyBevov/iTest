57dba5f513087f4ea9c7467f1a22f6f0b6da5439
Yuriy Bevov <jury19y@mail.ru>
2021-09-20 12:45:54 +0300

    почистил js

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -4,7 +4,4 @@ import modernizr from './vendors/modernizr.js';
 import parallax from './modules/parallax.js';
 import sliders from './modules/sliders.js';
 import aos from './modules/aos.js';
-
-//const scene = document.getElementById('scene');
-//const parallaxInstance = new Parallax(scene);
-
+ 
